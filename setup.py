a50248c9e44280844f14c6408b7db8c9184457fe
Flávio Briz <flavio@trocafone.com>
2016-07-07 11:55:06 -0300

    Structure done

---
+++ setup.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+# #############################################################################
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2016 Michell Stuttgart
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+###############################################################################
+from setuptools import setup, find_packages
+import os
+import re
+
+
+def get_version(package):
+    """
+    Based in https://github.com/tomchristie/django-rest-framework/blob/
+    971578ca345c3d3bae7fd93b87c41d43483b6f05/setup.py
+    :param package Package name
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+LONG_DESCRIPTION = open('README.md').read()
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
+
+setup(
+    name='correios-lib',
+    version=get_version('correios-lib'),
+    url='https://github.com/trocafone/correios-lib',
+    license='MIT License',
+    author='Flávio Briz',
+    author_email='flavio.briz@gmail.com',
+    keywords='correios sigep sigepweb reversa',
+    description='Interface python com logs para uso dos serviços'
+                ' fornecidos pelo SIGEPWeb dos Correios.',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'suds-py3==1.3.2.0',
+        'voluptuous==0.8.11'
+    ],
+    platforms=['any'],
+    classifiers=CLASSIFIERS,
+    test_suite='tests',
+)
